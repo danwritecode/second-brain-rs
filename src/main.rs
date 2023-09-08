88dac0658afca231fc3a9eb021a1df34da5b9cc1
danwritecode <dnelson4993@outlook.com>
2023-09-08 16:39:25 -0400

    started implementing web socket

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use axum::{
+    TypedHeader,
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
     routing::{get, post},
     Router,
-    response::{Html, IntoResponse, Result, Response}, http::{HeaderMap, header, StatusCode}, extract::Query
+    response::{Html, IntoResponse, Result, Response}, 
+    http::{HeaderMap, header, StatusCode}, 
+    extract::{Query, ConnectInfo, ws::CloseFrame}
 };
 
 use tower_livereload::LiveReloadLayer;
-use std::{net::SocketAddr, collections::HashMap};
+use std::{net::SocketAddr, collections::HashMap, borrow::Cow, ops::ControlFlow};
 use anyhow::anyhow;
 use serde::Deserialize;
 
@@ -27,6 +31,7 @@ async fn main() -> Result<(), AppError> {
     let app = Router::new()
         .route("/", get(root_route))
         .route("/search", get(search_route))
+        .route("/ws", get(ws_handler))
         .route("/css", get(css))
         .route("/js", get(js))
         .route("/route-name/uix/clicked", post(clicked_uix))
@@ -77,6 +82,78 @@ async fn search_route(Query(params): Query<HashMap<String, String>>) -> Result<H
     let rendered = render_with_global_context("search-route/index.html", &context)?;
     
     Ok(Html(rendered))
+}
+
+
+// async fn ws_handler(
+    // ws: WebSocketUpgrade,
+    // ConnectInfo(addr): ConnectInfo<SocketAddr>,
+// ) -> impl IntoResponse {
+//     println!("Websocket request received");
+//     ws.on_upgrade(move |socket| handle_socket(socket, addr))
+// }
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+) -> impl IntoResponse {
+    println!("Websocket request received");
+    ws.on_upgrade(move |socket| handle_socket(socket))
+}
+
+/// Actual websocket statemachine (one will be spawned per connection)
+async fn handle_socket(mut socket: WebSocket) {
+    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
+        println!("Pinged...");
+    } else {
+        println!("Could not send ping!");
+        return;
+    }
+    
+    tokio::spawn(async move {
+        while let Some(Ok(msg)) = socket.recv().await {
+            if process_message(msg).is_break() {
+                break;
+            }
+            socket.send(Message::Text("<p>response from ws</p>".to_string())).await.unwrap();
+        }
+    });
+
+    // returning from the handler closes the websocket connection
+    println!("Websocket context destroyed");
+}
+
+/// helper to print contents of messages to stdout. Has special treatment for Close.
+fn process_message(msg: Message) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(t) => {
+            println!(">>> received str: {:?}", t);
+        }
+        Message::Binary(d) => {
+            println!(">>> received {} bytes: {:?}", d.len(), d);
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> received close with code {} and reason `{}`",
+                    cf.code, cf.reason
+                );
+            } else {
+                println!(">>> somehow sent close message without CloseFrame");
+            }
+            return ControlFlow::Break(());
+        }
+
+        Message::Pong(v) => {
+            println!(">>> received pong with {:?}", v);
+        }
+        // You should never need to manually handle Message::Ping, as axum's websocket library
+        // will do so for you automagically by replying with Pong and copying the v according to
+        // spec. But if you need the contents of the pings you can see them here.
+        Message::Ping(v) => {
+            println!(">>> received ping with {:?}", v);
+        }
+    }
+    ControlFlow::Continue(())
 }
 
 async fn clicked_uix() -> Result<Html<String>, AppError> {
